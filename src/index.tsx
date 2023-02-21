80773d41542a025516e330faced31fb63f862c88
Reqvite <reqvite2233@gmail.com>
2023-02-21 23:53:57 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/lms">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
