7fb0d86f9e187882a09118e564aa00b46a10dc19
Reqvite <reqvite2233@gmail.com>
2023-02-28 21:28:15 +0200

    Update operations.ts

--- src/redux/auth/operations.ts
+++ src/redux/auth/operations.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://lms-api-eu.onrender.com";
 //localhost:3000
 //lms-api-eu.onrender.com
 
